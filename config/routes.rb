5efc7055732df83b262978dad2731e54dcecf06d
Sepehr Hosseini <iamsepehrhosseini@gmail.com>
2020-07-10 19:34:10 +0400

    feat(init): init project

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  resources :orders
+  resources :categories
+  resources :foods
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
